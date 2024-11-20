d0e6575740956b70aeadeffe9ec6be15722cc8a7
cswenshuo <cswenshuo@gmail.com>
2024-11-20 23:01:54 +0800

    删除掉sparkle框架 使用Contactview代替about 更换icon 去掉了排序设置 修复了popover弹出后不关闭问题

--- Maccy/Sorter.swift
+++ Maccy/Sorter.swift
@@ -1,52 +1,9 @@
-import AppKit
-import Defaults
+//
+//  Sorter.swift
+//  Maccy
+//
+//  Created by cswenshuo on 2024/11/20.
+//  Copyright © 2024 p0deje. All rights reserved.
+//
 
-// swiftlint:disable identifier_name
-// swiftlint:disable type_name
-class Sorter {
-  enum By: String, CaseIterable, Identifiable, CustomStringConvertible, Defaults.Serializable {
-    case lastCopiedAt
-    case firstCopiedAt
-    case numberOfCopies
-
-    var id: Self { self }
-
-    var description: String {
-      switch self {
-      case .lastCopiedAt:
-        return NSLocalizedString("LastCopiedAt", tableName: "StorageSettings", comment: "")
-      case .firstCopiedAt:
-        return NSLocalizedString("FirstCopiedAt", tableName: "StorageSettings", comment: "")
-      case .numberOfCopies:
-        return NSLocalizedString("NumberOfCopies", tableName: "StorageSettings", comment: "")
-      }
-    }
-  }
-
-  func sort(_ items: [HistoryItem], by: By = Defaults[.sortBy]) -> [HistoryItem] {
-    return items
-      .sorted(by: { return bySortingAlgorithm($0, $1, by) })
-      .sorted(by: byPinned)
-  }
-
-  private func bySortingAlgorithm(_ lhs: HistoryItem, _ rhs: HistoryItem, _ by: By) -> Bool {
-    switch by {
-    case .firstCopiedAt:
-      return lhs.firstCopiedAt > rhs.firstCopiedAt
-    case .numberOfCopies:
-      return lhs.numberOfCopies > rhs.numberOfCopies
-    default:
-      return lhs.lastCopiedAt > rhs.lastCopiedAt
-    }
-  }
-
-  private func byPinned(_ lhs: HistoryItem, _ rhs: HistoryItem) -> Bool {
-    if Defaults[.pinTo] == .bottom {
-      return (lhs.pin == nil) && (rhs.pin != nil)
-    } else {
-      return (lhs.pin != nil) && (rhs.pin == nil)
-    }
-  }
-}
-// swiftlint:enable identifier_name
-// swiftlint:enable type_name
+import Foundation
---
+++ Maccy/Views/ContactView.swift
@@ -0,0 +1,19 @@
+//
+//  ContactView.swift
+//  Maccy
+//
+//  Created by cswenshuo on 2024/11/20.
+//  Copyright © 2024 p0deje. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactView()
+}
